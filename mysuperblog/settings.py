505ef331d69063a0f69fa2f620f9da2418ac0b2e
Pierre-Emmanuel Brian <pebrian@outlook.fr>
2022-03-28 15:45:30 +0200

    flower config

--- mysuperblog/settings.py
+++ mysuperblog/settings.py
@@ -139,5 +139,5 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
 CACHE_TTL = 60 * 1
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
