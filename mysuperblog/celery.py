5377b8d615ebe1619b7616ca078f3ee584754ced
Pierre-Emmanuel Brian <pebrian@outlook.fr>
2022-03-28 14:45:16 +0200

    Celery working

--- mysuperblog/celery.py
+++ mysuperblog/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysuperblog.settings')
 
-app = Celery('app')
+app = Celery('mysuperblog')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
