09b366a593d573a32539814872c620206597a378
Pierre-Emmanuel Brian <pebrian@outlook.fr>
2022-03-29 13:37:00 +0200

    flower config

--- mysuperblog/celery.py
+++ mysuperblog/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysuperblog.settings')
 
-app = Celery('mysuperblog')
+app = Celery('mysuperblog', backend='redis')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
